b7468842ec6df40fb09589d694ac2c189a743991
weicong <weicong@yunweinetworks.com>
2022-02-23 18:22:38 +0800

    sys service

--- squirrel-framework/src/main/java/org/squirrel/framework/auth/SysController.java
+++ squirrel-framework/src/main/java/org/squirrel/framework/auth/SysController.java
@@ -2,18 +2,14 @@ package org.squirrel.framework.auth;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.squirrel.framework.database.DefaultBaseController;
+import org.springframework.web.bind.annotation.*;
+import org.squirrel.framework.cache.BaseCache;
 import org.squirrel.framework.response.Rp;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
- * <p>用户信息 Cotroller</p>
+ * <p>默认系统服务 Cotroller</p>
  * @author weicong
  * @time   2021-02-21 
  * @version v1
@@ -23,14 +19,27 @@ import java.util.List;
 @RequestMapping()
 public class SysController {
 
-	@Auth(AuthMenuLoader.ADD)
-	@ApiOperation(value = "登录")
-	@PostMapping(value = "login")
-	public Rp<AuthUser> add(@RequestBody AuthUser authUser) {
+	@Resource
+	private BaseCache baseCache;
 
-		return null;
+	@ApiOperation(value = "登录")
+	@PostMapping(value = "loginIn")
+	public Rp<AuthUser> loginIn(@RequestBody AuthUser authUser) {
+		// TODO 获取用户权限信息接口
+		AuthUser authUser1 = new AuthUser();
+		// TODO 随机ID
+		String token = "";
+		baseCache.put(token, authUser1, 20*60);
+		// TODO 返回数据待确定
+		return Rp.success();
 	}
 
+	@ApiOperation(value = "登出")
+	@GetMapping(value = "loginOut/{token}")
+	public Rp<String> loginOut(@PathVariable("token") String token) {
 
+//		baseCache.put("", authUser1, 20*60);
+		return Rp.success();
+	}
 
 }
